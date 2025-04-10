73be5d9e16fc1c7b51f91aac2ee334f73eaebc6b
Ananya Saxena <ana.saxena07@gmail.com>
2025-04-10 11:35:51 +0530

    commit

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-let 
+let express = require('express');
+
+let app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello to the world of Azure');
+}
+);
+
+const Port = 3000;
+
+app.listen(Port, () => {
+    console.log(`Server is running on port ${Port}`);
+});
